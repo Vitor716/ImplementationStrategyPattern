cde1710891240f217c310029672164d67d11b144
vitor <vitorvieira_deoliveira@hotmail.com>
2025-03-20 19:51:35 -0300

    initial commit

---
+++ src/main/java/com/example/implementationstrategypattern/ImplementationStrategyPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.implementationstrategypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationStrategyPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementationStrategyPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/implamentationstategypatten/ImplamentationStategyPattenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implamentationstategypatten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplamentationStategyPattenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
